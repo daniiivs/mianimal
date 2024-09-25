ba9caa83d8b961042df7b5b9a788774ac0c4f45d
Dani <danielvs90@gmail.com>
2024-09-25 20:23:28 +0100

    Formulario

---
+++ src/components/Ficha.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './App.css'
+import {useState} from "react"
+import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
+
+function Ficha() {
+    const datos = {
+        animal: 'gato',
+        imageUrl: '/catImage.png',
+        imageSize: 200
+    }
+    const [count, setCount] = useState(0)
+
+    function handleClick() {
+        setCount(count + 1)
+    }
+
+    return (
+        <>
+            <Stack
+                direction={{xs: 'column', sm: 'column', md: 'row'}}
+                sx={{alignItems: 'center', justifyContent: 'center'}}
+                spacing={{xs: 2, sm: 4, md: 6}}
+            >
+                <Typography variant='h2'>Soy un {datos.animal} vivo y feliz</Typography>
+                <Avatar src={datos.imageUrl} alt='Gato feliz'
+                        sx={{height: datos.imageSize, width: datos.imageSize}}/>
+                <Button color='primary' size='small' variant='text' onClick={handleClick}>Me
+                    acariciaste {count} veces</Button>
+                {/*<Button color='secondary' size='medium' variant='contained' onClick={handleClick}>Me
+                    acariciaste {count} veces</Button>
+                <Button color='success' size='large' variant='outlined' onClick={handleClick} disabled='true'>Me
+                    acariciaste {count} veces (deshabilitado)</Button>
+                <Button sx={{color: 'orange', backgroundColor: 'yellow'}} size='large' variant='contained'
+                        onClick={handleClick}>Letras naranjas y fondo amarillo</Button>
+                <Button sx={{color: 'red', backgroundColor: 'pink'}} size='large' variant='outlined'
+                        onClick={handleClick}>Letras rojas y fondo rosa</Button>*/}
+            </Stack>
+        </>
+    )
+}
+
+export default Ficha
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
-import Appgrid from "./Appgrid.jsx";
+import Appgrid from "./components/Appgrid.jsx";
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
